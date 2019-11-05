9f2455465c4b9a358a46c3905dd55c2bb868fd5d
chenhao.haochen <chenhao.haochen@alibaba-inc.com>
2019-11-05 21:35:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kanaries.github.io
---
+++ Rath/precache-manifest.e571831b0fa7c9e6c2c440f763214511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb67335aead15cb6c2a165cd22045fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c0085b9e0792b62638c",
+    "url": "/static/css/2.d1461702.chunk.css"
+  },
+  {
+    "revision": "b91f0475893c78d35127",
+    "url": "/static/css/main.bc7397d4.chunk.css"
+  },
+  {
+    "revision": "8c0085b9e0792b62638c",
+    "url": "/static/js/2.97a046c9.chunk.js"
+  },
+  {
+    "revision": "b91f0475893c78d35127",
+    "url": "/static/js/main.02ab1796.chunk.js"
+  },
+  {
+    "revision": "5ad6dc52180fca3143ed",
+    "url": "/static/js/runtime-main.a9e8376a.js"
+  }
+]);
